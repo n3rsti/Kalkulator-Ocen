95520d2333a1b631fd08aa4985b29b51a3d491b9
n3rsti <krzysiek.witucki55@gmail.com>
2019-01-17 18:55:53 +0100

    added basic logic and styles

---
+++ app.js
@@ -0,0 +1,50 @@
+// y is variable counting
+const calc = new Vue({
+    el: "#header",
+    data: {
+        marks: [
+            
+        ],
+        mark: [
+
+        ],
+        weights: [
+
+        ],
+        weight: [
+
+        ],
+        x: 0,
+        y: 0,
+        total: 0,
+        totalWeight: 0,
+        averageMark: 0,
+    },
+    
+    methods: {
+        addMark: function(){
+            const marks = document.querySelector(".marks");
+            
+            
+            
+            this.marks.push(this.mark[this.x]);
+            this.weights.push(this.weight[this.x])          
+            this.total += parseInt(this.marks[this.x] * this.weights[this.x]);
+            this.totalWeight += parseInt(this.weights[this.x]);
+            
+
+            for(var count = 0; count < this.marks.length; count++){
+                this.averageMark = this.total / this.totalWeight;
+            }
+
+
+            const newMark = document.createElement('div');
+            newMark.classList.add('mark');
+            const titleMark = document.createElement('h1')
+            titleMark.innerText = this.marks[this.x];
+            marks.append(newMark);
+            newMark.append(titleMark)
+            this.x++;
+        },
+    }
+});
